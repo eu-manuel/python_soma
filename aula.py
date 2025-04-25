e7be554de324551205de19591102896145f4fcc0
Aluno Lauro de Freitas <aluno.lauro@senaiba.com.br>
2025-04-25 19:06:43 -0300

    criação da função para somar

---
+++ aula.py
@@ -0,0 +1,8 @@
+def somar():
+
+    a = int(input("digite o primeiro numero: "))
+    b = int(input("digite o segundo numero: "))
+     
+    print(f"A soma dos valores digitados e igual a: {a+b}")
+
+somar()
